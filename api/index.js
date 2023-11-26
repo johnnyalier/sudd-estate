a4cd1a802cb37e0185c2407ed0a502b850d6ef3b
David Kenyi <davidkenyi@outlook.com>
2023-11-25 23:44:44 -0500

    Created sign up route and a middleware functions

--- api/index.js
+++ api/index.js
@@ -3,13 +3,26 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import userRouter from './routes/user.js'
+import authRouter from './routes/auth.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5556
 
+app.use(express.json())
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to sudd-estate database')
