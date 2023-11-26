75e05a2f8dcd2a84e0724fed48951c2b00c4893e
David Kenyi <davidkenyi@outlook.com>
2023-11-25 22:59:49 -0500

    Created API test route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+import userRouter from './routes/user.js'
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5556
 
+app.use('/api/user', userRouter);
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to sudd-estate database')
