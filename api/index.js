7f497673512934c18ccf47228e642d0c669ad970
David Kenyi <davidkenyi@outlook.com>
2023-11-25 21:49:36 -0500

    Connected API to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -6,6 +7,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5556
 
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}`);
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to sudd-estate database')
+    app.listen(PORT, () => {
+        console.log(`Server listening at port ${PORT}`);
+    })
 })
